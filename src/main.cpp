1922009ec4073979cd283daf91614959f60d3e49
s67-POKAWIN <s6701012610295@email.kmutnb.ac.th>
2024-12-01 12:52:13 +0700

    add project file

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <AccelStepper.h>
+// put function declarations here:
+int myFunction(int, int);
+
+#define Mode 1
+#define PUL 13
+#define DIR 12
+
+
+AccelStepper stepper(Mode,PUL,DIR); // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+
+void setup()
+{  
+  stepper.setMaxSpeed(1500);
+  stepper.setAcceleration(750);
+  stepper.runToNewPosition(500);
+}
+
+void loop()
+{    
+  stepper.runToNewPosition(100);
+  stepper.runToNewPosition(300);
+  stepper.runToNewPosition(10);
+}
